6843d2f4c130c7d8ac1aa0e6a058073aef71b7b0
Miguel Angel Berenguer Alcaine <mberenguer@addingtechnology.es>
2023-12-28 11:48:33 +0100

    Configuración tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        link: '#003564',
+        linkHover: '#0088FF',
+      },
+    },
+  },
+  plugins: [],
+}
+
